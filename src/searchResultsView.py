334133222adb264bf00d07fa08a2dac28c14206c
Shawn Axsom <shawn.axsom@rooksecurity.com>
2015-04-21 22:06:03 -0400

    Finished adding search keys in file, allowing search next and previous

--- src/searchResultsView.py
+++ src/searchResultsView.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import logging
 import os
 import urwid
 import subprocess
@@ -8,6 +9,10 @@ from searchResultsLinesBuilder import SearchResultsLinesBuilder
 class SearchResultsView(BaseView):
 
     def __init__(self, parameters_manager, view_manager):
+        LOG_FILENAME = 'logging_example.out'
+        logging.basicConfig(filename=LOG_FILENAME,
+                            level=logging.DEBUG,
+                            )
         self.parameters_manager = parameters_manager
         self.view_manager = view_manager
 
@@ -34,16 +39,45 @@ class SearchResultsView(BaseView):
         return builder.build()
 
     def on_keypress(self, input):
-        if input == '/':
+        if input in ('/', '?'):
+            self.view_manager.footer.clear()
             self.view_manager.main_frame.focus_part = 'footer'
         elif input == 'enter':
-            self.view_manager.main_frame.focus_part = 'body'
-            i = 0
-            for line in self.content:
-                if self.view_manager.footer.get_input() in line.original_widget.text:
-                    self.set_focus(i)
-                    break
-                i = i + 1
-            self.view_manager.footer.clear()
+            self.start_search()
+        elif input == 'n':
+            self.search_next(self.focus_position + 1)
+        elif input == 'N':
+            self.search_previous(self.focus_position - 1)
         else:
             super(SearchResultsView, self).on_keypress(input)
+
+    def start_search(self):
+        self.view_manager.main_frame.focus_part = 'body'
+        self.search_next(0)
+
+    def search_next(self, starting_position):
+        if starting_position > self.content or starting_position < 0:
+            return
+
+        i = starting_position
+        for line in self.content[i:len(self.content) - 1]:
+            if self.view_manager.footer.get_input() in line.original_widget.text:
+                self.set_focus(i)
+                break
+            i = i + 1
+
+    def search_previous(self, starting_position):
+        if starting_position > self.content or starting_position < 0:
+            return
+
+        i = starting_position
+        logging.info('Starting from: %s' % starting_position)
+
+        reversed_content = self.content[0:i+1][::-1]
+        for line in reversed_content:
+            logging.info('Line number: %s' % i)
+            logging.info('Line: %s' % line.original_widget.text)
+            if self.view_manager.footer.get_input() in line.original_widget.text:
+                self.set_focus(i)
+                break
+            i = i - 1
